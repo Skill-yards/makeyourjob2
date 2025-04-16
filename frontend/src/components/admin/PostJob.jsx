1d51f5672bc6cc4c3fe16a76f1c655060526e1e5
ShahilRaza <shahil4278@gmail.com>
2025-04-15 22:30:51 -0700

    some changes

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -16,7 +16,6 @@ import { useNavigate } from 'react-router-dom';
 import {
   Loader2,
   Briefcase,
-  MapPin,
   Check,
   ChevronsUpDown,
   Edit,
@@ -899,4 +898,4 @@ const PostJob = () => {
   );
 };
 
-export default PostJob;
+export default  PostJob;
